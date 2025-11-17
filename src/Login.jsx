ffede1c431f0dfd4d61221fb3beedff8bef75dc8
Juwurason <ajibolasunday7@gmail.com>
2025-11-17 21:13:53 +0100

    chore: loader changes

--- src/Login.jsx
+++ src/Login.jsx
@@ -70,16 +70,10 @@ export default function Login({ onLogin }) {
             disabled={loading} // 🔹 disable button while logging in
           >
             {loading ? (
-              <svg className="spinner" viewBox="0 0 50 50">
-                <circle
-                  className="path"
-                  cx="25"
-                  cy="25"
-                  r="20"
-                  fill="none"
-                  strokeWidth="5"
-                />
-              </svg>
+              <span className="loader-text">
+                Logging in...
+                <span className="loader-spinner"></span>
+              </span>
             ) : (
               "Login"
             )}
